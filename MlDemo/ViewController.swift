ca6d09ae82bc2a3c5a7c1d1799e327e4f2596ff3
Pushpinder Pal Singh <thisisme@pushpinderpalsingh.com>
2020-01-26 01:37:35 +0530

    Initial Commit

---
+++ MlDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MlDemo
+//
+//  Created by Pushpinder Pal Singh on 26/01/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
